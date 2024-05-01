a77eb570855aec6b78c73c6b6715b2920994abfa
jinno <klvx01@gmail.com>
2024-05-01 13:00:24 +0900

    fix: add open_interpreter memo

--- server.py
+++ server.py
@@ -1,5 +1,3 @@
-import os
-
 from dotenv import load_dotenv
 
 from interpreter.core.core import OpenInterpreter
@@ -10,17 +8,38 @@ from ui.server_impl_gradio import server
 # Load the users .env file into environment variables
 load_dotenv(verbose=True, override=False)
 
+# memo: 2024/05/01
+# structure of OpenInterpreter
+#   interpreter: top layer
+#       core:
+#           core.py(OpenInterpreter): 3 topic exist
+#               <llm main call sequence>
+#                    chat() -> _streaming_chat() -> _respond_and_store() -> respond() -> llm.run()
+#               <python code in system_message is run() via render_message() before llm>
+#                    If python code exist in message, respond() -> render_message() -> computer.run() before llm.
+#               <response sequence(use computer)>
+#                   _respond_and_store() -> respond() -> computer.run()
+#           computer: core functions like os, files, etc.
+#           os: run shell command via computer.run()
+#           terminal: run python command via computer.run()
+#           llm: run() use litellm via run_text_llm() or run_function_calling_llm()
+#               litellm: llm call
+#           server.py: server version. fastapi and uvicorn. call interpreter.chat()
+#           respond.py: respond()
+#       terminal_interface:
+#           terminal_interface: cli only version? call interpreter.chat()
+#   ui: gradio interface
+#       server_impl_gradio.py: call interpreter.chat()
+# interpreter: top layer
+
+# interpreter
 interpreter = OpenInterpreter(
     auto_run=True,
 )
-# interpreter.llm.model = "claude-3-haiku-20240307"
-# interpreter.llm.model = "anthropic/claude-3-haiku-20240307"
-interpreter.llm.model = "openrouter/anthropic/claude-3-haiku"
-# interpreter.llm.model = "openrouter/anthropic/claude-3-haiku:beta"
-interpreter.llm.api_base = os.getenv("API_BASE")
+interpreter.llm.model = "anthropic/claude-3-haiku-20240307"
+# interpreter.llm.model = "openrouter/anthropic/claude-3-haiku"
 print("api_base=", interpreter.llm.api_base)
 
-interpreter.llm.max_tokens = 2000
 interpreter.verbose = False
 interpreter.sync_computer = False
 interpreter.system_message += """
@@ -29,8 +48,15 @@ If you lost previous work, you should check this directory and result from files
 """
 interpreter.llm_drop_params = True
 interpreter.llm_modify_params = True
+
+# llm(litellm)
+llm = interpreter.llm
+llm.max_tokens = 2000
+
+# computer
 computer = interpreter.computer
 computer.debug = True
 computer.verbose = True
+computer.save_skills = True
 interpreter.llm.supports_functions = False
 server(interpreter)
--- ui/server_impl_gradio.py
+++ ui/server_impl_gradio.py
@@ -1,3 +1,4 @@
+import threading
 import time
 from functools import partial
 
@@ -6,26 +7,6 @@ from langchain.memory import ConversationBufferWindowMemory
 
 from .message_format import show_data_debug
 from .message_process import process_messages_gradio
-
-
-# チャットボットの応答を生成する関数
-def chat_old(
-    interpreter, memory: ConversationBufferWindowMemory, history, new_query: str
-):
-    show_data_debug(history, "history")
-    show_data_debug(new_query, "new_query")
-    history = history + [(new_query, None)]
-
-    # Start a separate task for processing messages
-    response = ""
-    for chunk in process_messages_gradio(new_query, interpreter, memory):
-        response += chunk["content"]
-        history[-1] = (history[-1][0], response)
-        yield history
-
-    # 最終的な応答を履歴に追加する
-    history[-1] = (history[-1][0], response)
-    yield history
 
 
 # チャットボットの応答を生成する関数
@@ -46,22 +27,6 @@ def chat(interpreter, memory: ConversationBufferWindowMemory, new_query: str, hi
 
     # 最終的な応答を履歴に追加する
     yield response
-
-
-def launch_interface_chat_single(interpreter, memory):
-    # 部分適用された関数を作成する
-    chat_with_llm = partial(chat, interpreter, memory)
-
-    iface = gr.Interface(
-        fn=chat_with_llm,
-        inputs=[
-            gr.Chatbot(value=[("ユーザー", "こんにちは")], label="チャット履歴"),
-            gr.Textbox(label="新しいメッセージ"),
-        ],
-        outputs=gr.Chatbot(label="更新されたチャット履歴"),
-    )
-    iface.launch(server_name="0.0.0.0")
-    auto_input_single(iface, chat_with_llm, interpreter, memory)
 
 
 def launch_interface_chat(interpreter, memory):
@@ -91,11 +56,19 @@ def launch_interface_chat(interpreter, memory):
         clear_btn="Clear",
     )
     iface.launch(server_name="0.0.0.0")
-    auto_input(iface)
+    start_auto_input(iface)
+
+
+def start_auto_input(iface):
+    # 自動入力スレッドを開始する
+    auto_input_thread = threading.Thread(target=auto_input, args=iface)
+    auto_input_thread.daemon = True
+    auto_input_thread.start()
 
 
 def auto_input(iface):
     while True:
+        print("auto_input start")
         time.sleep(30)
 
         # シミュレートされた入力を生成する
@@ -104,47 +77,7 @@ def auto_input(iface):
         # 模擬実行
         iface.textbox.value = simulated_input
         iface.textbox.submit()
-
-
-def auto_input_old(iface, chat_with_llm):
-    while True:
-        time.sleep(30)
-
-        # シミュレートされた入力を生成する
-        simulated_input = "会話履歴から状況を確認してから、自動的に処理を続けてください。"
-
-        # チャット履歴を取得する
-        history = iface.get_history()
-
-        # 結果を出力する
-        print("自動入力:", simulated_input)
-        final_new_history = None
-        for new_history in chat_with_llm(history, simulated_input):
-            final_new_history = new_history
-        print("結果:", final_new_history[-1][1])
-
-        # 更新された履歴をチャットインターフェースに適用する
-        iface.set_history(final_new_history)
-
-
-def auto_input_single(iface, chat_with_llm, interpreter, memory):
-    # 自動入力をシミュレートする
-    while True:
-        # 次の自動入力までの遅延を追加する
-        time.sleep(30)
-
-        simulated_input = "会話履歴から状況を確認して、自動的に処理を続けてください。"
-        history = iface.outputs[0].value
-
-        # 結果を出力する
-        print("自動入力:", simulated_input)
-        final_new_history = None
-        for new_history in chat_with_llm(history, simulated_input):
-            final_new_history = new_history
-        print("結果:", final_new_history[-1][1])
-
-        # 結果をGradioのUIに反映する
-        iface.outputs[0].update(value=final_new_history)
+        print("auto_input submit")
 
 
 def server(interpreter):
